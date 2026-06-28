3c3f9f4876d96e866580f2835119ddb395f2e8c8
Jonaline González <jonalinegonzalez2710@gmail.com>
2026-06-28 10:54:12 -0600

    Add salary calculation script in fee.py

---
+++ fee.py
@@ -0,0 +1,6 @@
+name=input("¿Como te llamas?")
+print("Hola",name) 
+hour=float(input("¿How many hours do you work per month?"))
+fee=float(input("¿How much do you earn per hour?"))
+total= hour * fee
+print("tu salario bruto al mes es",total)
